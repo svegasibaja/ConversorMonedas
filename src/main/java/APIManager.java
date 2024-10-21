a5e6594edf997ba654ba29af84eab367efa151a9
Sebastian <svegasibaja@gmail.com>
2024-10-20 20:54:38 -0500

    Finish

--- src/main/java/APIManager.java
+++ src/main/java/APIManager.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class APIManager {
 
-    private static final String API_KEY = "4d7d10e06c84ba8322876119";  // Reemplaza esto con tu propia API key
+    private static final String API_KEY = "4d7d10e06c84ba8322876119";
     private static final String BASE_URL = "https://v6.exchangerate-api.com/v6/" + API_KEY + "/latest/";
 
     public static double obtenerTasaCambio(String monedaOrigen, String monedaDestino) throws IOException {
